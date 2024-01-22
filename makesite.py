436008f22a5ea8d3ee9996eafa6bfa9a1c1b302a
Patrick Martin <pux1987@gmail.com>
2024-01-21 21:26:11 -0700

    More site updates, now I have a real about page!

--- makesite.py
+++ makesite.py
@@ -175,7 +175,7 @@ def main():
     # Default parameters.
     params = {
         'base_path': '',
-        'subtitle': 'Lorem Ipsum',
+        'subtitle': 'Mad Programming Experiments',
         'author': 'Patrick Martin',
         'site_url': 'http://localhost:8000',
         'current_year': datetime.datetime.now().year
