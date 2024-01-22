067895f21cfb38f3fefb44bc4e57aab9e6c42aa7
Patrick Martin <pux1987@gmail.com>
2024-01-21 20:03:40 -0700

    Working on exporting data from blogger

---
+++ blogger-parse.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import xml.etree.ElementTree as ET
+
+if __name__ == '__main__':
+    tree = ET.parse('blogger_export/feed.atom')
+    root = tree.getroot()
+    print('root: ', root.tag)
+    for entry in root.findall('{http://www.w3.org/2005/Atom}entry'):
+        print("Entry: ", entry.tag, ' :: attrib:', entry.attrib)
+        type = entry.find('{http://schemas.google.com/blogger/2018}type')
+        if type is None:
+            continue
+        status = entry.find('{http://schemas.google.com/blogger/2018}status')
+        if status is None:
+            continue
+
+        print('\tType', type.text)
+        print('\tStatus', status.text)
+
+        if type.text == 'POST' and status.text == 'LIVE':
+            title = entry.find('{http://www.w3.org/2005/Atom}title').text
+            published = entry.find('{http://www.w3.org/2005/Atom}published').text
+            content = entry.find('{http://www.w3.org/2005/Atom}content').text
+            print('Title: ', title)
+            print('Published: ', published)
+            # print('Content: ', content)
+
+            for elem in entry.iter():
+                print('\t', elem.tag)
